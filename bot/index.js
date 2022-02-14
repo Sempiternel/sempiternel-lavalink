61a2272b8611c084781dd3e874cb9d9d793ccb70
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-02-14 22:31:58 +0100

    add: event handling

--- bot/index.js
+++ bot/index.js
@@ -9,6 +9,28 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
 });
 
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
 client.manager = new Manager({
   plugins: [new Spotify(), new Filters()],
   nodes: [{ host: process.env.LAVALINK_HOST }],
@@ -35,41 +57,7 @@ client.manager = new Manager({
 
     player.destroy();
   });
-
-client.commands = new Collection();
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-}
-
-client.once("ready", () => {
-  console.log("Ready!");
-  client.manager.init(client.user.id);
-});
-
 client.on("raw", (d) => client.manager.updateVoiceState(d));
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
 
 dotenv.config();
 client.login(process.env.DISCORD_TOKEN);
